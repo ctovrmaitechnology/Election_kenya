b40abc199082b1b7da8b171521dcf10cd9517efb
ctovrmaitechnology <cto@vrmaitechnology.com>
2026-07-27 14:59:09 +0530

    GitHub Pages setup

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,10 @@
 // export default defineConfig({
 //   plugins: [react()]
 // });
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
   base: '/Election_kenya/',
-});
+})
